74125cc88ac679cc97c5bd2162519e05b605c22a
metodo5fasi-wq <metodo5fasi@gmail.com>
2026-06-09 17:38:48 +0200

    Delete PianoAlimentare.jsx

--- PianoAlimentare.jsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://hdgiwrwcxfbojqfeyrxn.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkZ2l3cndjeGZib2pxZmV5cnhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NTUxNzMsImV4cCI6MjA5NjMzMTE3M30.Ul4cEtYBxj0EmSASokj0jZAJnRa9JKXygfzl0838sAo'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
