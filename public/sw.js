547c5dfd36ba3986024c3101b63cc928e63a64d6
metodo5fasi-wq <metodo5fasi@gmail.com>
2026-06-11 08:09:27 +0200

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'fofit-v1'
+self.addEventListener('install', e => {
+  self.skipWaiting()
+})
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim())
+})
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request).catch(() => caches.match('/index.html')))
+  }
+})
